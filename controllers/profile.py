bb983ffed740f1927085ff9af6e8c482b4d0df4c
suresh <suresh12_dmm@yahoo.com>
2019-04-11 10:36:52 +0100

    add base paths for profile

---
+++ controllers/profile.py
@@ -0,0 +1,96 @@
+import logging
+import template_engine
+from library.user import UserLibrary
+from library.account import AccountHelper
+
+class ProfileController:
+    @classmethod
+    def show_profile(cls, request):
+        request.response.headers['Content-Type'] = 'text/html'
+        user = UserLibrary.get_user(request)
+        logging.info(user['user'])
+        template_values = {
+            'url': user["url"],
+            'url_string': user['url_string'],
+            'user': user['user']
+        }
+
+        template = template_engine.JINJA_ENVIRONMENT.get_template('views/twitter/profile.html')
+        request.response.write(template.render(template_values))
+
+    @classmethod
+    def save_tweet(cls, request):
+        request.response.headers['Content-Type'] = 'text/html'
+        user = UserLibrary.get_user(request)
+
+        msg = ""
+        data = {
+            'url': user["url"],
+            'url_string': user['url_string'],
+            'user': user['user'],
+            'msg': msg
+        }
+        template = template_engine.JINJA_ENVIRONMENT.get_template('views/twitter/profile.html')
+        request.response.write(template.render(data))
+
+    @classmethod
+    def update_tweet(cls, request):
+        request.response.headers['Content-Type'] = 'text/html'
+        user = UserLibrary.get_user(request)
+
+        msg = ""
+        data = {
+            'url': user["url"],
+            'url_string': user['url_string'],
+            'user': user['user'],
+            'msg': msg
+        }
+        template = template_engine.JINJA_ENVIRONMENT.get_template('views/twitter/profile.html')
+        request.response.write(template.render(data))
+
+    @classmethod
+    def delete_tweet(cls, request):
+        request.response.headers['Content-Type'] = 'text/html'
+        user = UserLibrary.get_user(request)
+
+        msg = ""
+        data = {
+            'url': user["url"],
+            'url_string': user['url_string'],
+            'user': user['user'],
+            'msg': msg
+        }
+        template = template_engine.JINJA_ENVIRONMENT.get_template('views/twitter/profile.html')
+        request.response.write(template.render(data))
+
+    @classmethod
+    def search_tweet(cls, request):
+        request.response.headers['Content-Type'] = 'text/html'
+        user = UserLibrary.get_user(request)
+
+        msg = ""
+        data = {
+            'url': user["url"],
+            'url_string': user['url_string'],
+            'user': user['user'],
+            'msg': msg
+        }
+        template = template_engine.JINJA_ENVIRONMENT.get_template('views/twitter/profile.html')
+        request.response.write(template.render(data))
+
+    @classmethod
+    def search_user(cls, request):
+        request.response.headers['Content-Type'] = 'text/html'
+        user = UserLibrary.get_user(request)
+
+        msg = ""
+        data = {
+            'url': user["url"],
+            'url_string': user['url_string'],
+            'user': user['user'],
+            'msg': msg
+        }
+        template = template_engine.JINJA_ENVIRONMENT.get_template('views/twitter/profile.html')
+        request.response.write(template.render(data))
+
+
---
+++ request/profile.py
@@ -0,0 +1,29 @@
+import webapp2
+
+import logging
+from controllers.profile import ProfileController
+
+class ProfileRequest(webapp2.RequestHandler):
+    def get(self):
+        path = self.request.path
+        if path == "/profile":
+            AccountController.update_profile_get(self)
+
+    def post(self):
+        form_name = self.request.get('form')
+        if form_name == "save_tweet":
+            ProfileController.save_tweet(self)
+        elif form_name == "update_tweet":
+            ProfileController.update_tweet(self)
+        elif form_name == "delete_tweet":
+            ProfileController.delete_tweet(self)
+        elif form_name == "search_tweet":
+            ProfileController.search_tweet(self)
+        elif form_name == "search_user":
+            ProfileController.search_user(self)
+
+
+
+app = webapp2.WSGIApplication([
+    ('/profile', ProfileRequest),
+], debug=True)
